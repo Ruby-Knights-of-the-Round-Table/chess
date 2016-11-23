407e8b64e074fbff6f8dfc11c7ab10771dfcc826
lalalilu <lilulu004@gmail.com>
2016-11-23 10:47:36 -0500

    Add devise to application with signin signout profile-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :players
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root "menus#index"
+
+  resources :players, only: :show
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
