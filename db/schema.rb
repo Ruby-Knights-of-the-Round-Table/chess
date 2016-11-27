4dffcc8b4365cbb9d273f8753b4543aebf810c38
chiyingleung1 <chiying.leung@gmail.com>
2016-11-27 18:57:28 -0500

    Add piece model and set up STI

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123153015) do
+ActiveRecord::Schema.define(version: 20161127224406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pieces", force: true do |t|
+    t.integer  "game_id"
+    t.integer  "player_id"
+    t.string   "type"
+    t.integer  "x_position"
+    t.integer  "y_position"
+    t.boolean  "selected",       default: false
+    t.boolean  "captured_piece", default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "players", force: true do |t|
     t.string   "email",                  default: "", null: false
