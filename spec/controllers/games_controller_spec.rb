32194bbe99b81406047b8e42169ae36ddb2dbe76
chiyingleung1 <chiying.leung@gmail.com>
2016-12-03 13:29:29 -0500

    Add rspec test for games create action

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
   describe "games#new" do
-    it "should require users to be signed in" do
+    it "should require players to be signed in" do
       get :new
       expect(response).to redirect_to new_player_session_path
     end
@@ -13,4 +13,24 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "games#create" do
+    it "should require players to be signed in" do
+      post :create, game: { game_name: "Test Game"}
+      expect(response).to redirect_to new_player_session_path
+    end
+    it "should allow players to successfully create a new game in the database" do
+      player = FactoryGirl.create(:player)
+      sign_in player
+
+      game = FactoryGirl.create(:game)
+      post :create, game_id: game.id, game: { game_name: "Test Game"}
+      expect(response).to redirect_to game_path(assigns[:game])
+
+      game = Game.last
+      expect(game.game_name).to eq("Test Game")
+      expect(game.white_player_id).to eq(player.id)
+    end
+  end
+
 end
