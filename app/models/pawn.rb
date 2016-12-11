3f11a1275b7e6d7fc703d38630fc2994e82a8637
Patrick Hagmann <phagmann1@gmail.com>
2016-12-11 16:43:12 +0000

    Caching problem fix and rename color to symbol

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,6 +1,6 @@
 class Pawn < Piece
-    def color
-        if Game.find(self.game_id).white_player_id === self.player_id then return "&#9817;" else return "&#9823;" end
+    def symbol
+        if self.game.white_player_id == self.player_id then return "&#9817;" else return "&#9823;" end
     end
 
     def piece_can_move_to(board)
--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,6 +1,6 @@
 class Rook < Piece
-    def color
-        if Game.find(self.game_id).white_player_id === self.player_id then return "&#9814;" else return "&#9820;" end
+    def symbol
+        if self.game.white_player_id == self.player_id then return "&#9814;" else return "&#9820;" end
     end
 
     def piece_can_move_to(board)
