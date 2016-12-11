7bb140c8778ca66adc5ce380cf32f2f0931315ec
vagrant <vagrant@precise32.(none)>
2016-12-11 20:19:59 +0000

    allow rook to move horizontal and vertical

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -3,6 +3,29 @@ class Rook < Piece
         if Game.find(self.game_id).white_player_id === self.player_id then return "&#9814;" else return "&#9820;" end
     end
 
+    def piece_can_move_to(board)
+        final_spots = []
+        rook_move_shape = []
+
+        #horizontal and vertical
+        ((-7..7).to_a - [0]).each do |i|
+          rook_move_shape << [0,i]
+          rook_move_shape << [i,0]
+        end
+
+        curr_x  = self.x_position
+        curr_y = self.y_position
+        rook_move_shape.each do |spot|
+            x = spot[0]
+            y = spot[1]
+            possible_x = curr_x + x
+            possible_y = curr_y + y
+
+            final_spots << [possible_x,possible_y] if (possible_x >= 0 && possible_y >= 0 && possible_x <= 7 && possible_y <= 7 ) && board[possible_y][possible_x] === 0
+            # figure out if position is in bounds of board AND no pieces are there
+        end
+        return final_spots
+    end
     # def piece_can_move_to(board)
 
         # piece logic goes inside this method
