b67623fd8605d969ae7f4ddb8596defb7ce65726
Chase Bristow <cbristow26@gmail.com>
2016-12-12 19:15:50 +0000

    Bishop piece_can_move_to

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,5 +1,40 @@
 class Bishop < Piece
-    def color
-        if Game.find(self.game_id).white_player_id === self.player_id then return "&#9815;" else return "&#9821;" end
+    def symbol
+        if self.game.white_player_id == self.player_id then return "&#9813;" else return "&#9819;" end
     end
+
+    def piece_can_move_to(board)
+      final_spots = []
+      bishop_move_shape = []
+
+      #diagonal moves
+      ((-7..7).to_a - [0]).each do |i|
+        bishop_move_shape << [i,i]
+        bishop_move_shape << [i,-i]
+      end
+
+      curr_y = self.y_position
+      curr_x  = self.x_position
+      bishop_move_shape.each do |spot|
+           y = spot[0]
+           x = spot[1]
+           possible_y = curr_y + y
+           possible_x = curr_x + x
+           final_spots << [possible_y,possible_x] if (possible_x >= 0 && possible_y >= 0 && possible_x <= 7 && possible_y <= 7 ) && board[possible_y][possible_x] === 0
+           # figure out if position is in bounds of board AND no pieces are there
+      end
+      return final_spots
+    end
+
 end
+
+# curr_x  = self.x_position
+# curr_y = self.y_position
+# queen_move_shape.each do |spot|
+#      x = spot[0]
+#      y = spot[1]
+#      possible_x = curr_x + x
+#      possible_y = curr_y + y
+#      final_spots << [possible_x,possible_y] if (possible_x >= 0 && possible_y >= 0 && possible_x <= 7 && possible_y <= 7 ) && board[possible_y][possible_x] === 0
+#      # figure out if position is in bounds of board AND no pieces are there
+# end
