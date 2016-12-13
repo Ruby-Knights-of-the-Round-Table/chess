ff312ec8212c748814b025f6ddf3910461055dbb
lalalilu <lilulu004@gmail.com>
2016-12-13 13:50:11 -0500

    Diagonal obstruction

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -10,21 +10,51 @@ class Piece < ActiveRecord::Base
   def not_obstructed(board,final_spots)
     # returns an array that selected piece can move to, given obstructions
     # vertical
-      max = 7 - y_position
-      min = y_position
-      (-min..max).each do |i|
-        if board[y_position+i][x_position] != nil && board[y_position+i][x_position] > 0 && (y_position+i) >= 0 && (y_position+i) <= 7
+    max = 7 - y_position
+    min = y_position
+    (-min..max).each do |i|
+      if board[y_position+i][x_position] != nil && board[y_position+i][x_position] > 0 && (y_position+i) >= 0 && (y_position+i) <= 7
 
-          if i > 0
-            final_spots.delete_if { |coord| coord[0] > (y_position+i) && coord[1] == x_position }
-            next
-          elsif i < 0
-            final_spots.delete_if { |coord| coord[0] < (y_position+i) && coord[1] == x_position }
-          end
-
+        if i > 0
+          final_spots.delete_if { |coord| coord[0] > (y_position+i) && coord[1] == x_position }
+          next
+        elsif i < 0
+          final_spots.delete_if { |coord| coord[0] < (y_position+i) && coord[1] == x_position }
         end
+
       end
-      return final_spots
+    end
+
+    # diagonal
+    max = 7 - y_position
+    min = y_position
+    (-min..max).each do |i|
+      if board[y_position+i][x_position+i] != nil && board[y_position+i][x_position+i] > 0 && (y_position+i) >= 0 && (y_position+i) <= 7 && (x_position+i) >= 0 && (x_position+i) <= 7
+        if i > 0
+          final_spots.delete_if { |coord| coord[0] > (y_position+i) && coord[1] > (x_position+i) }
+          next
+        elsif i < 0
+          final_spots.delete_if { |coord| coord[0] < (y_position+i) && coord[1] < (x_position+i) }
+        end
+
+      end
+    end
+    # max = 7 - y_position
+    # min = y_position
+    (-min..max).each do |i|
+      if board[y_position+i][x_position-i] != nil && board[y_position+i][x_position-i] > 0 && (y_position+i) >= 0 && (y_position+i) <= 7 && (x_position+i) >= 0 && (x_position+i) <= 7
+        if i > 0
+          final_spots.delete_if { |coord| coord[0] > (y_position+i) && coord[1] > (x_position-i) }
+          next
+        elsif i < 0
+          final_spots.delete_if { |coord| coord[0] < (y_position+i) && coord[1] < (x_position+i) }
+        end
+
+      end
+    end
+
+
+    return final_spots
   end
 
   # placeholder only ... remove after all piece logic is completed
