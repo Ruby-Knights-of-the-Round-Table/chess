0204cdbff9c1bb7bd831731e51d10b199db00ce3
lalalilu <lilulu004@gmail.com>
2016-12-28 08:46:43 -0500

    Pawn capture #2

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -5,47 +5,18 @@ class Pawn < Piece
 
     def piece_can_move_to(board)
       final_spots = []
-
-      if occupied_space?(y_position+1,x_position+1) || occupied_space?(y_position+1,x_position-1)
-        pawn_capture_shape = [ [1,1], [1,-1] ]
-        curr_y = self.y_position
-        curr_x  = self.x_position
-        pawn_capture_shape.each do |spot|
-          y = spot[0]
-          x = spot[1]
-          possible_y = curr_y + y
-          possible_x = curr_x + x
-          final_spots << [possible_y,possible_x] if (possible_x >= 0 && possible_y >= 0 && possible_x <= 7 && possible_y <= 7 ) && board[possible_y][possible_x] != self.player_id
-        end
-        return not_obstructed(board,final_spots)
-
-      elsif occupied_space?(y_position-1,x_position+1) || occupied_space?(y_position-1,x_position-1)
-          pawn_capture_shape = [ [-1,1], [-1,-1] ]
-          curr_y = self.y_position
-          curr_x  = self.x_position
-          pawn_capture_shape.each do |spot|
-            y = spot[0]
-            x = spot[1]
-            possible_y = curr_y + y
-            possible_x = curr_x + x
-            final_spots << [possible_y,possible_x] if (possible_x >= 0 && possible_y >= 0 && possible_x <= 7 && possible_y <= 7 ) && board[possible_y][possible_x] != self.player_id
-          end
-          return not_obstructed(board,final_spots)
-
-
-      elsif game.white_player_id == player_id
+      if game.white_player_id == player_id
         final_spots << [y_position+2, x_position] if y_position == 1
         final_spots << [y_position+1, x_position]
-        return not_obstructed(board,final_spots)
-
+        final_spots << [y_position+1, x_position+1] if occupied_space?(y_position+1,x_position+1)
+        final_spots << [y_position+1, x_position-1] if occupied_space?(y_position+1,x_position-1)
       else
         final_spots << [y_position-2, x_position] if y_position == 6
         final_spots << [y_position-1, x_position]
-        return not_obstructed(board,final_spots)
-
+        final_spots << [y_position-1, x_position+1] if occupied_space?(y_position-1,x_position+1)
+        final_spots << [y_position-1, x_position-1] if occupied_space?(y_position-1,x_position-1)
       end
-
-
+      not_obstructed(board,final_spots)
     end
 
 
