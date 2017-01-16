e3124bdd954e2726c385d8708aa4baa41a18952b
chiyingleung1 <chiying.leung@gmail.com>
2017-01-16 16:12:27 -0500

    Fix supercheck div; should not show when winner_id is 0

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,14 +20,14 @@
 
 function moveLog(pieceID) {
     $(function () {
-      console.log(pieceID)
+      // console.log(pieceID)
       $.get( '/moves/' + pieceID.toString()).success (function(infomation)  {
-      console.log(infomation)
+      // console.log(infomation)
       var div = document.getElementById("log");
       div.innerHTML = "<p> Moves List: </p>"
       for( var i = 0; i < infomation.length; i++){
-        currentMove = infomation[i] 
-        if( parseInt(currentMove[1]) == 0 ){ 
+        currentMove = infomation[i]
+        if( parseInt(currentMove[1]) == 0 ){
             continue
         }
         else{
@@ -37,10 +37,10 @@ function moveLog(pieceID) {
             else{
                 div.innerHTML += "<p> Turn " + currentMove[1]+  ": Black " + currentMove[0] + " to y: " + currentMove[3] + ", x: " + currentMove[4] +"</p>";
             }
-     
+
         }
 
-        
+
       }
 
       })
